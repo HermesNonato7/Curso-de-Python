9802fb38ca648dcd6ddb5489f847e7e4e93e70df
HermesNonato7 <hermes.nonato7@hotmail.com>
2022-10-30 22:44:35 -0300

    primeiro commit

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+# Adição
+print(1 + 1)
+
+# Subtração
+print(10 - 2)
+
+# Multiplicação
+print(4 * 3)
+
+# Divisão
+print(12 / 3)
+
+# Divisão interia
+print(12 // 2)
+
+# Módulo
+print(10 % 3)
+
+# Exponenciação
+print(2 ** 3)
---
+++ operadores_associacao.py
@@ -0,0 +1,6 @@
+frutas = ["limao", "uva"]
+curso = "Curso de Python"
+
+print("laranja" in frutas)
+print("limao" not in frutas)
+print("Python" in curso)
