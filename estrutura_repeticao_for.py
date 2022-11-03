09bec8c61b6d4aa354d73248ec9da1e83362917a
HermesNonato7 <hermes.nonato7@hotmail.com>
2022-11-03 11:36:14 -0300

    primeiro commit

---
+++ estrutura_repeticao_for.py
@@ -0,0 +1,9 @@
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+else:
+    print() # adiciona uma quebra de linha
+    print("Executa no final do laço")
---
+++ estruturas_repeticao.py
@@ -0,0 +1,10 @@
+# Recebe um número do teclado e exiba os 2  números seguintes
+
+a = int(input("Informe um número inteiro: "))
+print(a)
+
+a += 1
+print(a)
+
+a += 1 
+print(a)
